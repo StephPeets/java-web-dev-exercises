8c262e40b17d1a4d9f54b63c682fd8b568bda5cc
Steph Peets <stephanie.peets@gmail.com>
2021-09-19 16:41:25 -0400

    complete Ch 2, Ex 3

---
+++ src/exercises/NumericTypes.java
@@ -0,0 +1,23 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class NumericTypes {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Miles driven:  ");
+
+        double milesDriven = input.nextDouble();
+
+        System.out.println("Gallons of gas used:  ");
+
+        double gasUsed = input.nextDouble();
+
+        double milesPerGallon = milesDriven / gasUsed;
+
+        System.out.printf("Miles driven: %s\nGallons of gas: %s\n\tMiles per gallon: %s", milesDriven, gasUsed, milesPerGallon);
+    }
+}
