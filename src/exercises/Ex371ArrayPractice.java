bcf8fd6f538906b09e5a24504b6c06c44b06e777
Steph Peets <stephanie.peets@gmail.com>
2021-09-22 22:31:21 -0400

    minor reformatting

--- src/exercises/Ex371ArrayPractice.java
+++ src/exercises/Ex371ArrayPractice.java
@@ -3,30 +3,29 @@ package exercises;
 import java.util.Arrays;
 
 public class Ex371ArrayPractice {
-    public static void main(String[] args) {
 
-        int[] arr371 = new int[6];
-        arr371[0] = 1;
-        arr371[1] = 1;
-        arr371[2] = 2;
-        arr371[3] = 3;
-        arr371[4] = 5;
-        arr371[5] = 8;
+  public static void main(String[] args) {
+    int[] arr371 = new int[6];
 
-        int[] arr3711 = {
-                1, 1, 2, 3, 5, 8
-        };
+    arr371[0] = 1;
+    arr371[1] = 1;
+    arr371[2] = 2;
+    arr371[3] = 3;
+    arr371[4] = 5;
+    arr371[5] = 8;
 
-        for (int num : arr371) {
-            System.out.println(num);
-        }
+    int[] arr3711 = { 1, 1, 2, 3, 5, 8 };
 
-        System.out.println(Arrays.toString(arr371));
-        System.out.println(Arrays.toString(arr3711));
-
-        System.out.println(arr371.length);
-
-        System.out.println(arr371.length);
-
+    for (int num : arr371) {
+      System.out.println(num);
     }
+
+    System.out.println(Arrays.toString(arr371));
+    
+    System.out.println(Arrays.toString(arr3711));
+
+    System.out.println(arr371.length);
+
+    System.out.println(arr371.length);
+  }
 }
