7d31bf2ca6492a3178a81d0c3f6357b646092327
Steph Peets <stephanie.peets@gmail.com>
2021-09-24 23:26:44 -0400

    formatting

--- src/org/launchcode/java/demos/lsn2controlflowandcollections/HashMapGradebook.java
+++ src/org/launchcode/java/demos/lsn2controlflowandcollections/HashMapGradebook.java
@@ -6,42 +6,39 @@ import java.util.Scanner;
 
 public class HashMapGradebook {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    HashMap<String, Double> students = new HashMap<>();
+    Scanner input = new Scanner(System.in);
+    String newStudent;
 
-        HashMap<String, Double> students = new HashMap<>();
-        Scanner input = new Scanner(System.in);
-        String newStudent;
+    System.out.println("Enter your students (or ENTER to finish):");
 
-        System.out.println("Enter your students (or ENTER to finish):");
+    // Get student names and grades
+    do {
+      System.out.print("Student: ");
+      newStudent = input.nextLine();
 
-        // Get student names and grades
-        do {
+      if (!newStudent.equals("")) {
+        System.out.print("Grade: ");
+        Double newGrade = input.nextDouble();
+        students.put(newStudent, newGrade);
 
-            System.out.print("Student: ");
-            newStudent = input.nextLine();
+        // Read in the newline before looping back
+        input.nextLine();
+      }
+    } while (!newStudent.equals(""));
 
-            if (!newStudent.equals("")) {
-                System.out.print("Grade: ");
-                Double newGrade = input.nextDouble();
-                students.put(newStudent, newGrade);
+    // Print class roster
+    System.out.println("\nClass roster:");
+    double sum = 0.0;
 
-                // Read in the newline before looping back
-                input.nextLine();
-                input.close();
-            }
-
-        } while(!newStudent.equals(""));
-
-        // Print class roster
-        System.out.println("\nClass roster:");
-        double sum = 0.0;
-
-        for (Map.Entry<String, Double> student : students.entrySet()) {
-            System.out.println(student.getKey() + " (" + student.getValue() + ")");
-            sum += student.getValue();
-        }
-
-        double avg = sum / students.size();
-        System.out.println("Average grade: " + avg);
+    for (Map.Entry<String, Double> student : students.entrySet()) {
+      System.out.println(student.getKey() + " (" + student.getValue() + ")");
+      sum += student.getValue();
     }
+
+    double avg = sum / students.size();
+    System.out.println("Average grade: " + avg);
+    input.close();
+  }
 }
