f4155714014430965c82b77e2ee731da4fabf6f8
Steph Peets <stephanie.peets@gmail.com>
2021-09-25 01:44:11 -0400

    complete Ex 3.7.1

--- src/exercises/Ex3713SplitArray.java
+++ src/exercises/Ex3713SplitArray.java
@@ -1,10 +1,20 @@
 package exercises;
 
+import java.util.Arrays;
+
 public class Ex3713SplitArray {
 
-    public static void main (String[] args) {
+  public static void main(String[] args) {
+    String saucy =
+      "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse";
 
+    String[] saucyArray = saucy.split("\\. ");
 
+    System.out.println(Arrays.toString(saucyArray));
+    
+    for ( String sauce : saucyArray) {
+        System.out.println(sauce + ".");
 
     }
+  }
 }
